c62ad5c412e4ef0485661549fd149719ba3d7a8c
Anwar Hussen <anwar.hussen4@gmail.com>
2020-11-19 22:37:56 +0600

    initial setup done

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-row">
+    <div class="bg-red-200">Left</div>
+    <div class="bg-blue-300 flex-grow">Right</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
